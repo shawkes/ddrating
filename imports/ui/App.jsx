8878e2cc54dd2d28b76ac845c035189982011742
shawkes <shawkes@gmail.com>
2017-03-27 10:24:35 +0100

    Empty React app

---
+++ imports/ui/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Rate us</h1>
+        </header>
+
+        <ul>
+          <li>Ace</li>
+        </ul>
+      </div>
+    );
+  }
+}

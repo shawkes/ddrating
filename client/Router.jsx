b9ee0924d6c247223d865db3ca006ad103f725de
shawkes <shawkes@gmail.com>
2017-03-27 22:04:32 +0100

    Basic ratings api

--- client/Router.jsx
+++ client/Router.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { mount } from 'react-mounter';
 import App from '../imports/ui/App.jsx';
 import Rater from '../imports/ui/Rater.jsx';
 import Dashboard from '../imports/ui/Dashboard.jsx';
-import { mount } from 'react-mounter';
 
 FlowRouter.route('/', {
   action() {
--- imports/ui/Dashboard.jsx
+++ imports/ui/Dashboard.jsx
@@ -1,10 +1,28 @@
 import React, { PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
 
-export default class Dashboard extends React.Component {
+import { Ratings } from '../api/ratings.js';
+
+class Dashboard extends React.Component {
   render() {
     return (
-      <div >nope
+      <div >Number of ratings {this.props.ratings.length}
       </div>
     );
   }
 }
+
+// App.propTypes = {
+//   ratings: PropTypes.array(
+//     PropTypes.shape({
+//       start: PropTypes.number.isRequired,
+//       end: PropTypes.number.isRequired,
+//     })
+//   ).isRequired,
+// };
+
+export default createContainer(() => {
+  return {
+    ratings: Ratings.find({}).fetch(),
+  };
+}, Dashboard);
